1cb5f8e4f6ed1b150cd67d549f4e14037c13d06f
mhnasajpour <mhnasajpour@gmail.com>
2023-04-19 19:05:19 +0330

    Add check constraints to tables

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -20,9 +20,9 @@ create table Category(
 
 create table Member(
 	member_id		int identity(1,1) primary key,
-	email			varchar(50) unique not null,
+	email			varchar(50) unique not null check(email like '%@%.%'),
 	password		varchar(50) not null,
-	phone_number	varchar(11) null,
+	phone_number	varchar(11) null check(phone_number like '09%' and len(phone_number) = 11),
 	thumbnail		nvarchar(MAX) null,
 	date_created	datetime default getdate(),
 	date_updated	datetime default getdate()
@@ -41,7 +41,7 @@ create table "User"(
 	"user_id"		int primary key,
 	first_name		nvarchar(50) not null,
 	last_name		nvarchar(50) not null,
-	birth_date		date null,
+	birth_date		date null check(birth_date < getdate()),
 	province		nvarchar(50) null,
 	date_created	datetime default getdate(),
 	date_updated	datetime default getdate(),
@@ -53,9 +53,9 @@ create table Event(
 	category			nvarchar(50) null,
 	organizer_id		int not null,
 	name				nvarchar(50) not null,
-	price				decimal(12, 3) null,
+	price				decimal(12, 3) null check(price >= 0),
 	place				nvarchar(50) null,			
-	type				nvarchar(20) not null,
+	type				nvarchar(15) not null check(type in ('Attendance', 'Non-Attendance', 'Both')),
 	description			nvarchar(500) null,
 	exp_registration	datetime null,
 	date_created	datetime default getdate(),
@@ -88,7 +88,7 @@ create table Message(
 create table Discount(
 	code			nvarchar(20) primary key,
 	event_id		int not null,
-	"percent"		decimal(2, 0) not null,
+	"percent"		decimal(2, 0) not null check("percent" >= 0),
 	exp_date		datetime null,
 	date_created	datetime default getdate(),
 	date_updated	datetime default getdate(),
@@ -112,7 +112,7 @@ create table Ticket(
 	"user_id"		int not null,
 	event_id		int not null,
 	discount_code	nvarchar(20) null,
-	payment			decimal(12, 3) null,
+	payment			decimal(12, 3) null check(payment >= 0),
 	date_created	datetime default getdate(),
 	date_updated	datetime default getdate(),
 	foreign key ("user_id") REFERENCES "User"("user_id"),
@@ -135,7 +135,7 @@ create table Comment(
 create table Star(
 	"user_id"		int,
 	event_id		int,
-	rate			decimal(2, 1) not null,
+	rate			decimal(2, 1) not null check(rate between 0 and 5),
 	date_created	datetime default getdate(),
 	date_updated	datetime default getdate(),
 	primary key ("user_id", event_id),
@@ -144,42 +144,42 @@ create table Star(
 );
 
 
-insert into Category(title) values('Educational')
-insert into Category(title) values('Sports')
+insert into Category(title) values('Educational');
+insert into Category(title) values('Sports');
 
-insert into Member(email, password, phone_number, thumbnail) values('mhnasajpour@gmail.com', 'dfg64f', '09134596626', '/images/pic1')
-insert into Member(email, password, phone_number, thumbnail) values('danny@gmail.com', 'ikjhgb666', '09135649685', '/images/pic2')
-insert into Member(email, password, phone_number) values('melika@gmail.com', 'jhgf7d6sa54', '09387860245')
-insert into Member(email, password, phone_number, thumbnail) values('shayan@gmail.com', 'fdytjdfg898', '09107860245', '/images/pic3')
-insert into Member(email, password, phone_number) values('hellohackers@gmail.com', 'abcf767454', '09387860245')
-insert into Member(email, password, phone_number, thumbnail) values('aicup@gmail.com', 'ikqb0086', '09453671085', '/images/pic4')
+insert into Member(email, password, phone_number, thumbnail) values('mhnasajpour@gmail.com', 'dfg64f', '09134596626', '/images/pic1');
+insert into Member(email, password, phone_number, thumbnail) values('danny@gmail.com', 'ikjhgb666', '09135649685', '/images/pic2');
+insert into Member(email, password, phone_number) values('melika@gmail.com', 'jhgf7d6sa54', '09107826623');
+insert into Member(email, password, phone_number, thumbnail) values('shayan@gmail.com', 'fdytjdfg898', '09107860245', '/images/pic3');
+insert into Member(email, password, phone_number) values('hellohackers@gmail.com', 'abcf767454', '09387860245');
+insert into Member(email, password, phone_number, thumbnail) values('aicup@gmail.com', 'ikqb0086', '09453671085', '/images/pic4');
 
-insert into Organizer(organizer_id, name) values (3, 'Isfahan university of technology')
-insert into Organizer(organizer_id, name) values (4, 'Sepahan')
+insert into Organizer(organizer_id, name) values (3, 'Isfahan university of technology');
+insert into Organizer(organizer_id, name) values (4, 'Sepahan');
 
-insert into "User"("user_id", first_name, last_name) values(1, 'Mohammad', 'Nasajpour')
-insert into "User"("user_id", first_name, last_name) values(2, 'Danial', 'Khorasanizadeh')
+insert into "User"("user_id", first_name, last_name, birth_date) values(1, 'Mohammad', 'Nasajpour', '2002-03-27');
+insert into "User"("user_id", first_name, last_name) values(2, 'Danial', 'Khorasanizadeh');
 
-insert into Event(event_id, category, organizer_id, name, type, price) values(5, 'Educational', 3, 'Hello Hackers', 'Non-attendance', 100000)
-insert into Event(event_id, category, organizer_id, name, type, price) values(6, 'Educational', 3, 'AICup', 'Attendance', 200000)
+insert into Event(event_id, category, organizer_id, name, type, price) values(5, 'Educational', 3, 'Hello Hackers', 'Non-Attendance', 100000);
+insert into Event(event_id, category, organizer_id, name, type, price) values(6, 'Educational', 3, 'AICup', 'Attendance', 200000);
 
-insert into Social_media(member_id, name, link) values(1, 'Telegram', '@mhnasajpour')
-insert into Social_media(member_id, name, link) values(3, 'Linkedin', 'linkedin.com/in/fotoohi/')
+insert into Social_media(member_id, name, link) values(1, 'Telegram', '@mhnasajpour');
+insert into Social_media(member_id, name, link) values(3, 'Linkedin', 'linkedin.com/in/fotoohi/');
 
-insert into Message(member_id, text, is_seen) values(1, 'Welome to Evand', 1)
-insert into Message(member_id, text, is_seen) values(2, 'How can i help you?', 0)
+insert into Message(member_id, text, is_seen) values(1, 'Welome to Evand', 1);
+insert into Message(member_id, text, is_seen) values(2, 'How can i help you?', 0);
 
-insert into Discount(code, event_id, "percent") values ('6s7d8f5', 5, 50)
-insert into Discount(code, event_id, "percent", exp_date) values ('uihngbd', 5, 85, DATEADD(day, 7, GETDATE()))
+insert into Discount(code, event_id, "percent") values ('6s7d8f5', 5, 50);
+insert into Discount(code, event_id, "percent", exp_date) values ('uihngbd', 5, 85, DATEADD(day, 7, GETDATE()));
 
-insert into Staff("user_id", event_id, roll, is_admin) values (1, 6, 'HR', 0)
-insert into Staff("user_id", event_id, roll, is_admin) values (2, 5, 'CTO', 1)
+insert into Staff("user_id", event_id, roll, is_admin) values (1, 6, 'HR', 0);
+insert into Staff("user_id", event_id, roll, is_admin) values (2, 5, 'CTO', 1);
 
-insert into Ticket(serial_number, "user_id", event_id, discount_code, payment) values ('avnkewrj', 1, 5, '6s7d8f5', 50000)
-insert into Ticket(serial_number, "user_id", event_id, payment) values ('utjfkoif', 2, 6, 200000)
+insert into Ticket(serial_number, "user_id", event_id, discount_code, payment) values ('avnkewrj', 1, 5, '6s7d8f5', 50000);
+insert into Ticket(serial_number, "user_id", event_id, payment) values ('utjfkoif', 2, 6, 200000);
 
-insert into Comment("user_id", event_id, text, is_enabled) values (1, 5, 'It was very good', 1)
-insert into Comment("user_id", event_id, text, is_enabled) values (2, 5, 'What was this shit', 0)
+insert into Comment("user_id", event_id, text, is_enabled) values (1, 5, 'It was very good', 1);
+insert into Comment("user_id", event_id, text, is_enabled) values (2, 5, 'What was this shit', 0);
 
-insert into Star("user_id", event_id, rate) values (1, 5, 4.5)
-insert into Star("user_id", event_id, rate) values (2, 5, 1)
+insert into Star("user_id", event_id, rate) values (1, 5, 4.5);
+insert into Star("user_id", event_id, rate) values (2, 5, 1);
